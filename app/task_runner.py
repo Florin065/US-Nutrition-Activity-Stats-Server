2dd24c7d0b0df2f2bcd3b0c6f231f3169c8ac72f
Florin <subtirica.florin29@gmail.com>
2024-04-14 00:43:24 +0300

    Added implementation for ThreadPool and TaskRunner classes.

--- app/task_runner.py
+++ app/task_runner.py
@@ -1,28 +1,88 @@
+import multiprocessing
 from queue import Queue
-from threading import Thread, Event
-import time
+from threading import Event, Thread
+import json
+import os
 
 class ThreadPool:
     def __init__(self):
-        # You must implement a ThreadPool of TaskRunners
-        # Your ThreadPool should check if an environment variable TP_NUM_OF_THREADS is defined
-        # If the env var is defined, that is the number of threads to be used by the thread pool
-        # Otherwise, you are to use what the hardware concurrency allows
-        # You are free to write your implementation as you see fit, but
-        # You must NOT:
-        #   * create more threads than the hardware concurrency allows
-        #   * recreate threads for each task
-        pass
+        self.shutdown_event = Event()
+        self.task_queue = Queue()
+        self.job_status = {}
+        self.init_task_runners()
+
+        os.makedirs("data/jobs", exist_ok=True)
+
+    def init_task_runners(self):
+        num_threads = self.get_num_of_threads()
+        self.taskRunners = [TaskRunner(self.task_queue, self.shutdown_event, self.job_status) for _ in range(num_threads)]
+        for taskRunner in self.taskRunners:
+            taskRunner.start()
+
+    def get_num_of_threads(self) -> int:
+        default_num_of_threads = max(multiprocessing.cpu_count() - 1, 1)
+        num_of_threads = os.environ.get("TP_NUM_OF_THREADS")
+        
+        if num_of_threads:
+            try:
+                num_of_threads = int(num_of_threads)
+                if num_of_threads > 0:
+                    return num_of_threads
+            except ValueError:
+                pass
+        
+        return default_num_of_threads
+
+    def add_job(self, operation, *args):
+        if self.shutdown_event.is_set():
+            return None
+
+        job_id = self.task_queue.qsize()
+        self.task_queue.put((operation, args, job_id))
+        self.job_status[job_id] = "queued"
+
+        return job_id
+
+    def get_job(self, jobId):
+        status = self.job_status.get(jobId)
+        if status == "queued":
+            return {"status": "queued"}
+        elif status == "running":
+            return {"status": "running"}
+        
+        path = f"data/jobs/{jobId}.json"
+        if not os.path.exists(path):
+            return {"status": "error", "reason": "Invalid job_id"}
+        return json.load(open(path, "r"))
+
+    def graceful_shutdown(self):
+        self.shutdown_event.set()
+        
+        for taskRunner in self.taskRunners:
+            taskRunner.join()
 
 class TaskRunner(Thread):
-    def __init__(self):
-        # TODO: init necessary data structures
-        pass
+    def __init__(self, task_queue, shutdown_event, job_status):
+        super().__init__()
+        self.task_queue = task_queue
+        self.shutdown_event = shutdown_event
+        self.job_status = job_status
 
     def run(self):
-        while True:
-            # TODO
-            # Get pending job
-            # Execute the job and save the result to disk
-            # Repeat until graceful_shutdown
-            pass
+        while not self.shutdown_event.is_set():
+            try:
+                task, args, jobId = self.task_queue.get(timeout=1)
+            except:
+                continue
+
+            self.job_status[jobId] = "running"
+
+            result = task(*args)
+            self.save_result(jobId, result)
+
+            del self.job_status[jobId]
+            self.task_queue.task_done()
+
+    def save_result(self, job_id, result):
+        with open(f"data/jobs/{job_id}.json", "w") as f:
+            json.dump({"status": "done", "data": result}, f)
